fd991d360e529f02b21c9474a43ae7e2c3cc84ff
Jesus M. Castagnetto <jesus@castagnetto.com>
2019-11-13 11:05:55 -0500

    added code to generate a CSV file that includes the signature and set length

--- anagrams.py
+++ anagrams.py
@@ -1,7 +1,8 @@
 # Original code at: https://www.johndcook.com/blog/2019/11/13/anagram-frequency/
 from collections import defaultdict
 
-lines = open("spanish", "r").readlines()
+# change to the path on your system
+lines = open("/usr/share/dict/spanish", "r").readlines()
 
 words = set()
 for line in lines:
@@ -15,9 +16,21 @@ d = defaultdict(set)
 for w in words:
     d[sig(w)].add(w)
 
+# make a csv file containing the signature, length of set and its elements
+sig_file = open("anagrams-signature-spanish.csv", "w")
+print("signature,n,elements", file = sig_file)
+for s, wrds in d.items():
+    if len(wrds) > 1:
+        print("{},{},{}".format(s, str(len(wrds)), "|".join(wrds)), file = sig_file)
+sig_file.close()
+
+# make the anagram dictionary file
+dict_file = open("anagrams-spanish.txt", "w")
 for w in sorted(words):
     anas = sorted(d[sig(w)])
     if len(anas) > 1:
         anas.remove(w)
-        print("{}: {}".format(w, ", ".join(anas)))
+        print("{}: {}".format(w, ", ".join(anas)), file = dict_file)
+dict_file.close()
+
 
